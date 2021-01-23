d3c4bcb2688b45ce1e25d4998b1f5e7030320f8b
ninja <paramount_zhang@163.com>
2021-01-23 16:20:33 +0800

    old info

---
+++ ninja-maven-parent/ninja-maven-son/src/main/java/com/example/NinjaMavenSonApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaMavenSonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinjaMavenSonApplication.class, args);
+    }
+
+}
