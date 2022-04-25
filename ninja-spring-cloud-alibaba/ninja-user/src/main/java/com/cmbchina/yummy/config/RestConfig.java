b5e7b0f89c07d65bb5a55d5f55c1b0125f1dfbc4
ninja <paramount_zhang@163.com>
2022-04-25 22:03:56 +0800

    alibaba nacos

---
+++ ninja-spring-cloud-alibaba/ninja-user/src/main/java/com/cmbchina/yummy/config/RestConfig.java
@@ -0,0 +1,21 @@
+package com.cmbchina.yummy.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Desc
+ * @Author ninja
+ * @Date Created on 2021/5/4
+ */
+@Configuration
+public class RestConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
