589749816480da85eb6dbb55a3d818147c173c69
ninja <paramount_zhang@163.com>
2024-01-06 21:22:27 +0800

    sleuth+zipkin

--- ninja-spring-cloud-parent/feign-provider/src/main/java/com/ninja/controller/Controller.java
+++ ninja-spring-cloud-parent/feign-provider/src/main/java/com/ninja/controller/Controller.java
@@ -1,6 +1,8 @@
 package com.ninja.controller;
 
 import com.ninja.remote.client.VoucherRemoteClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -13,8 +15,13 @@ import java.util.UUID;
 @RestController
 public class Controller implements VoucherRemoteClient {
 
+    static Logger logger = LoggerFactory.getLogger(Controller.class);
+
     @Override
     public String getVouchers(Long userId) {
+
+        logger.info("开始模拟生成userID");
+
         if (userId == 1) {
             try {
                 //int sleepTime = new Random().nextInt(2000);
