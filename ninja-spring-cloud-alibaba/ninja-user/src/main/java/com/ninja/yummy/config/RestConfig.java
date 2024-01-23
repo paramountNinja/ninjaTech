0e8a12aafdbdc3f816c0408aa2fb45469bb08b4a
ninja <paramount_zhang@163.com>
2024-01-23 21:33:36 +0800

    nacos

--- ninja-spring-cloud-alibaba/ninja-user/src/main/java/com/ninja/yummy/config/RestConfig.java
+++ ninja-spring-cloud-alibaba/ninja-user/src/main/java/com/ninja/yummy/config/RestConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * @Desc
+ * @Desc RestConfig
  * @Author ninja
  * @Date Created on 2021/5/4
  */
@@ -16,6 +16,12 @@ public class RestConfig {
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+
+    @Bean
+    public RestTemplate restTemplateNoLoadBalanced() {
         return new RestTemplate();
     }
 }
--- ninja-spring-cloud-alibaba/ninja-user/src/main/java/com/ninja/yummy/controller/UserController.java
+++ ninja-spring-cloud-alibaba/ninja-user/src/main/java/com/ninja/yummy/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.ninja.yummy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +21,30 @@ public class UserController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private RestTemplate restTemplateNoLoadBalanced;
+
     @GetMapping("/getUserOrders/{userId}")
     public List<String> getUserOrders(@PathVariable("userId") String userId) {
         String url = "http://ninja-order/getOrders/" + userId;
         List<String> result = restTemplate.getForObject(url, List.class);
         return result;
     }
+
+    //=====以下写法很麻烦，可以使用@LoadBalanced注解替换，参加上面=========
+
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @GetMapping("/getUserOrders2/{userId}")
+    public List<String> getUserOrders2(@PathVariable("userId") String userId) {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("ninja-order");
+        String url = String.format("http://%s:%s/getOrders/%s",
+                serviceInstance.getHost(), serviceInstance.getPort(), userId);
+
+        List<String> result2 = restTemplateNoLoadBalanced.getForObject(url, List.class);
+        return result2;
+    }
+
 }
